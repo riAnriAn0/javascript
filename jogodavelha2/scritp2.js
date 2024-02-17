c3808f807e1fb73b158e06c35a9e1c71302b3cd3
riAnriAn0 <156357069+riAnriAn0@users.noreply.github.com>
2024-02-17 14:15:04 -0300

    edit11

--- jogodavelha2/scritp2.js
+++ jogodavelha2/scritp2.js
@@ -3,9 +3,13 @@ let local = document.querySelectorAll('.local')
 let jogadoro = document.querySelector('#jogadoro')
 let comecar = document.querySelector('.comecar')
 let cont = 1;
-let x = ('<i class="fa-solid fa-x"></i>')
-let o = ('<i class="fa-regular fa-circle"></i>')
-let empate = ('EMPATE')
+const x = ('<i class="fa-solid fa-x"></i>')
+const o = ('<i class="fa-regular fa-circle"></i>')
+let  fundov = document.querySelector('.fundov')
+let  fundo= document.querySelector('.fundo')
+let  nomeVencedor= document.querySelector('.vencedor')
+let vencedor = (0)
+let jgv = document.querySelector('.jgv')
 
 
 
@@ -15,85 +19,106 @@ comecar.addEventListener('click',() => {
 })
 
 function sistemadejogo(){
-        jogadorx.setAttribute('class','areajg')
-        local.forEach(function(div) {div.addEventListener('click',function(event) {
-                var buttonClicked = event.target;
-                cont += 1
-                if ( cont%2 == 0 ){
-                    buttonClicked.innerHTML = x;
-                    jogadoro.setAttribute('class', 'areajg')
-                    jogadorx.setAttribute('class','none')
-                } else{
-                    buttonClicked.innerHTML = o;
-                    jogadorx.setAttribute('class', 'areajg')
-                    jogadoro.setAttribute('class','none')
-                }
-                if ( cont == 10){
+    jogadorx.setAttribute('class','areajg')
+    local.forEach((div) => {div.addEventListener('click',(event) => {
+        var buttonClicked = event.target;
+        cont += 1
+        if ( cont%2 == 0 ){
+            buttonClicked.innerHTML = x;
+            jogadoro.setAttribute('class', 'areajg')
+            jogadorx.setAttribute('class','none')
+        } else{
+            buttonClicked.innerHTML = o;
+            jogadorx.setAttribute('class', 'areajg')
+            jogadoro.setAttribute('class','none')
+        }
+        if ( cont >= 10){
 
-                    window.open('index4.html', '_self')
+            vencedor = (3)
 
-                } else if(um.innerHTML == (x) && dois.innerHTML==(x) && tres.innerHTML == (x)){
-                    window.open('index.html', '_self') 
-                } else if (quatro.innerHTML == (x) && cinco.innerHTML== (x) && seis.innerHTML == (x)){
+        } else if(um.innerHTML == (x) && dois.innerHTML==(x) && tres.innerHTML == (x)){
 
-                     window.open('index.html', '_self') 
+            vencedor = (1) 
 
-                } else if (sete.innerHTML == (x) && oito.innerHTML== (x) && nove.innerHTML == (x)){
+        } else if (quatro.innerHTML == (x) && cinco.innerHTML== (x) && seis.innerHTML == (x)){
 
-                     window.open('index.html', '_self')
+            vencedor = (1) 
 
-                } else if (um.innerHTML == (x) && quatro.innerHTML== (x) && sete.innerHTML == (x)){
+        } else if (sete.innerHTML == (x) && oito.innerHTML== (x) && nove.innerHTML == (x)){
 
-                         window.open('index.html', '_self')
+            vencedor = (1)
 
-                } else if (cinco.innerHTML == (x) && dois.innerHTML== (x) && oito.innerHTML == (x)){
+        } else if (um.innerHTML == (x) && quatro.innerHTML== (x) && sete.innerHTML == (x)){
 
-                     window.open('index.html', '_self')
+            vencedor = (1)
 
-                } else if (tres.innerHTML == (x) && seis.innerHTML== (x) && nove.innerHTML == (x)){
+        } else if (cinco.innerHTML == (x) && dois.innerHTML== (x) && oito.innerHTML == (x)){
 
-                         window.open('index.html', '_self')
+            vencedor = (1)
 
-                } else if (um.innerHTML == (x) && cinco.innerHTML== (x) && nove.innerHTML == (x)){
+         } else if (tres.innerHTML == (x) && seis.innerHTML== (x) && nove.innerHTML == (x)){
 
-                         window.open('index.html', '_self')
+            vencedor = (1)
 
-                } else if (cinco.innerHTML == (x) && sete.innerHTML== (x) && tres.innerHTML == (x)){
+        } else if (um.innerHTML == (x) && cinco.innerHTML== (x) && nove.innerHTML == (x)){
 
-                         window.open('index.html', '_self') 
+            vencedor = (1)
 
-                } else if(um.innerHTML == (o) && dois.innerHTML== (o) && tres.innerHTML == (o)){
+        } else if (cinco.innerHTML == (x) && sete.innerHTML== (x) && tres.innerHTML == (x)){
 
-                window.open('index.3', '_self')
+            vencedor = (1) 
 
-                } else if (quatro.innerHTML == (o) && cinco.innerHTML== (o) && seis.innerHTML == (o)){
+        } else if (um.innerHTML == (o) && dois.innerHTML== (o) && tres.innerHTML == (o)){
 
-                    window.open('index3.html', '_self') 
+            vencedor = (2)
 
-                } else if (sete.innerHTML == (o) && oito.innerHTML== (o) && nove.innerHTML == (o)){  
+        } else if (quatro.innerHTML == (o) && cinco.innerHTML== (o) && seis.innerHTML == (o)){
 
-                    window.open('index3.html', '_self')
+            vencedor =  (2) 
 
-                } else if (um.innerHTML == (o) && quatro.innerHTML== (o) && sete.innerHTML == (o)){
+        } else if (sete.innerHTML == (o) && oito.innerHTML== (o) && nove.innerHTML == (o)){  
 
-                        window.open('index3.html', '_self')
+            vencedor =  (2)
 
-                } else if (cinco.innerHTML == (o) && dois.innerHTML== (o) && oito.innerHTML == (o)){
+        } else if (um.innerHTML == (o) && quatro.innerHTML== (o) && sete.innerHTML == (o)){
 
-                    window.open('index3.html', '_self')
+            vencedor =  (2)
 
-                } else if (tres.innerHTML == (o) && seis.innerHTML== (o) && nove.innerHTML == (o)){
+        } else if (cinco.innerHTML == (o) && dois.innerHTML== (o) && oito.innerHTML == (o)){
 
-                        window.open('index3.html', '_self')
+            vencedor =  (2)
 
-                } else if (um.innerHTML == (o) && cinco.innerHTML== (o) && nove.innerHTML == (o)){
+        } else if (tres.innerHTML == (o) && seis.innerHTML== (o) && nove.innerHTML == (o)){
 
-                        window.open('index3.html', '_self')
+            vencedor =  (2)
 
-                } else if (cinco.innerHTML == (o) && sete.innerHTML== (o) && tres.innerHTML == (o)){
-                    
-                    window.open('index3.html', '_self')      
-                } 
-        });
+        } else if (um.innerHTML == (o) && cinco.innerHTML== (o) && nove.innerHTML == (o)){
+
+            vencedor =  (2)
+
+        } else if (cinco.innerHTML == (o) && sete.innerHTML== (o) && tres.innerHTML == (o)){
+            
+            vencedor =  (2)      
+        } 
+
+        switch(vencedor){
+
+            case 1:
+                fundov.setAttribute('style','display: block')
+                fundo.setAttribute('style','display: none')
+                jgv.innerHTML = '<i class="fa-regular fa-x"></i> <br> VENCEDOR '
+                break;
+            case 2:
+                fundov.setAttribute('style','display: block')
+                fundo.setAttribute('style','display: none')  
+                jgv.innerHTML = '<i class="fa-regular fa-circle"></i> <br> VENCEDOR '                
+                break;
+            case 3 :
+                fundov.setAttribute('style','display: block')
+                fundo.setAttribute('style','display: none')
+                jgv.innerHTML = '<i class="fa-regular fa-x"></i> <i class="fa-regular fa-circle"></i> <br> EMPATE '             
+                break;
+        }
+    });
     });
 }
